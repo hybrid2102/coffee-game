d411d8742eadb0e2cd30323143d23a5d7d403d36
hybrid2102 <diannimarco@gmail.com>
2023-01-10 16:25:02 +0100

    fix for https://reactjs.org/docs/strict-mode.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { Redirect, Route, Switch } from "react-router";
@@ -11,13 +11,15 @@ import { ErrorPage } from "./components/ErrorPage";
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Game} />
-        <Route path="/help" component={Help} />
-        <Route path="/about" component={About} />
-        <Route path="*" component={ErrorPage} />
-      </Switch>
+      <StrictMode>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Game} />
+          <Route path="/help" component={Help} />
+          <Route path="/about" component={About} />
+          <Route path="*" component={ErrorPage} />
+        </Switch>
+      </StrictMode>
     </BrowserRouter>
   );
 }
--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="alert alert-warning" role="alert">
+      Ops... La pagina richiesta non esiste! Torna alla <Link to="/">Home</Link>
+      .
+    </div>
+  );
 };
