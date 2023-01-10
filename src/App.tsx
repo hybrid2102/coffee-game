d094913f3fd2e85a89819df222bb6cfe5f1ca16f
hybrid2102 <diannimarco@gmail.com>
2023-01-10 15:21:07 +0100

    react router 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router";
 import { Game } from "./components/Game";
 import { Header } from "./components/Header";
+import { About } from "./components/About";
+import { Help } from "./components/Help";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div className="text-center">
         <Header />
-        <Game />
+        <Switch>
+          <Route path="/" exact component={Game} />
+          <Route path="/help" component={Help} />
+          <Route path="/about" component={About} />
+        </Switch>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
