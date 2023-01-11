8ecf4359ff2d691bd57df6b902b2751695dbf09c
hybrid2102 <privatemail@mail.com>
2023-01-11 12:41:26 +0100

    help e about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const About = () => {
-  return <div>About</div>;
+  // Jan 4, 2023
+  return (
+    <div>
+      <h1 className="display-6 fw-bold text-center">Coffee Game</h1>
+      <div className="col-lg-7 mx-auto text-center">
+        <p className="lead mb-4">
+          Lo sviluppo di questo gioco è iniziato Mercoledì 4 Gennaio 2022 con lo
+          scopo di studiare React.
+        </p>
+      </div>
+    </div>
+  );
 };
