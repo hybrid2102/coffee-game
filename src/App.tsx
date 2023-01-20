bc1128ea9b9c8c6e5f14e40ac8be9308270ea71e
hybrid2102 <diannimarco@gmail.com>
2023-01-20 14:17:41 +0100

    commit 2/2 per git casing

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Switch } from "react-router";
 import { Game } from "./pages/Game/Game";
-import { Header } from "./rename1/Header";
+import { Header } from "./components/Header";
 import { About } from "./pages/About";
 import { Help } from "./pages/Help";
 import { NotFound } from "./pages/NotFound";
-import { PageLayout } from "./rename1/PageLayout";
+import { PageLayout } from "./components/PageLayout";
 
 export interface GameSettings {
   defaultMin: number;
