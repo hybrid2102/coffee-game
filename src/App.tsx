29d517075388327c9c1da37ff631fa81b754d3e7
hybrid2102 <diannimarco@gmail.com>
2023-01-20 14:10:52 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Switch } from "react-router";
 import { Game } from "./pages/Game/Game";
-import { Header } from "./components/Header";
+import { Header } from "./componentse/Header";
 import { About } from "./pages/About";
 import { Help } from "./pages/Help";
 import { NotFound } from "./pages/NotFound";
-import { PageLayout } from "./components/PageLayout";
+import { PageLayout } from "./componentse/PageLayout";
 
 export interface GameSettings {
   defaultMin: number;
