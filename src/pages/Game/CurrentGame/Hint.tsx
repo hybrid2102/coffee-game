e9963fa3deb9b3518dfb25c555ebb213634ba565
hybrid2102 <privatemail@mail.com>
2023-02-01 09:32:14 +0100

    wip hint bozza funzionamento

--- src/pages/Game/CurrentGame/Hint.tsx
+++ src/pages/Game/CurrentGame/Hint.tsx
@@ -6,23 +6,37 @@ export const Hint = (props: { range: GameRange }) => {
   const { range } = props;
   const { defaultRange } = useContext(GameContext);
 
-  const gridColumnsValue = `${range.min}% 1fr ${100 - range.max}%`;
+  const leftPc = (range.min * 100) / defaultRange.max;
+  const rightPc = 100 - (range.max * 100) / defaultRange.max;
+
+  const guessRange = 100 - rightPc - leftPc;
+  let rangeTemp = "center-cold";
+  if (guessRange < 50) {
+    rangeTemp = "center-warm";
+  }
+  if (guessRange < 25) {
+    rangeTemp = "center-hot";
+  }
+
+  const gridColumnsValue = `${leftPc}% 1fr ${rightPc}%`;
 
   return (
-    /*  <div className="alert alert-warning" role="alert">
-      Il numero segreto è compreso tra <strong>{range.min}</strong> e
-      <strong> {range.max}</strong> (esclusi)
-    </div> */
-    <div
-      className="container mb-3"
-      style={{
-        display: "grid",
-        gridTemplateColumns: gridColumnsValue,
-      }}
-    >
-      <div className="left"></div>
-      <div className="center"></div>
-      <div className="right"></div>
-    </div>
+    <>
+      <div className="alert alert-warning" role="alert">
+        Il numero segreto è compreso tra <strong>{range.min}</strong> e
+        <strong> {range.max}</strong> (esclusi)
+      </div>
+      <div
+        className="container mb-3"
+        style={{
+          display: "grid",
+          gridTemplateColumns: gridColumnsValue,
+        }}
+      >
+        <div className="left"></div>
+        <div className={rangeTemp}></div>
+        <div className="right"></div>
+      </div>
+    </>
   );
 };
